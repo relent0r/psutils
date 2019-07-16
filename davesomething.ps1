4f2cb56946b2e463edb51f9d284ce1451b04c2c1
Aaron Warner <aaronpaulwarner@gmail.com>
2019-07-16 22:09:03 +1200

    fo dave

---
+++ davesomething.ps1
@@ -0,0 +1,19 @@
+﻿$file = 'C:\temp\Book1.csv'
+$list = import-csv $file
+$newlist = @()
+ForEach ($li in $list)
+{
+  $number = $li.Number
+  $name = $li.Name
+  $x = 0
+  for ($x=1; $x -le $li.Number; $x++) 
+  {
+  $detail = @{ Name = $li.Name }
+  $newlist+= new-object  PSObject -Property $detail
+  write-host $newlist.Name
+  }
+  $newlist | export-csv -Path 'c:\temp\fuckit.csv' -NoTypeInformation
+  
+
+
+}
